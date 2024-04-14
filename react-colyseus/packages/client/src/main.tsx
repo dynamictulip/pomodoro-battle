da4b982fc3260319191b568d03fd187aaa05a8e8
Sara Gowen <me@dynamictulip.com>
2024-04-14 16:01:48 +0100

    Add font directly to repo

--- react-colyseus/packages/client/src/main.tsx
+++ react-colyseus/packages/client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import "nes.css/css/nes.min.css";
+import "./fonts/PressStart2P-Regular.ttf"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
