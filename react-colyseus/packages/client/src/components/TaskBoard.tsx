e18f71e32f550599502580e2c7db4cbfde7f7f6a
Sara Gowen <me@dynamictulip.com>
2024-04-14 17:03:31 +0100

    Tried to keep bulbasaur on the page. it's a bit better but still definitely broken

--- react-colyseus/packages/client/src/components/TaskBoard.tsx
+++ react-colyseus/packages/client/src/components/TaskBoard.tsx
@@ -5,9 +5,33 @@ import { Task } from './Tasks/Task';
 import { useAuthenticatedContext } from '../hooks/useAuthenticatedContext';
 export function TaskBoard() {
 
+    const ref: React.MutableRefObject<null | HTMLDivElement> = React.useRef(null);
+    const [width, setWidth] = React.useState(0);
+    const [height, setHeight] = React.useState(0);
+    React.useEffect(() => {
+        const { offsetWidth, offsetHeight, clientHeight, clientWidth } = ref.current as HTMLDivElement;
+        setWidth(offsetWidth);
+        setHeight(offsetHeight);
+
+        console.log('offsetWidth: ' + offsetWidth)
+        console.log('offsetHeight: ' + offsetHeight)
+        console.log('clientWidth: ' + clientWidth)
+        console.log('clientHeight: ' + clientHeight)
+    }, []);
+
+    const leeway = 16
+    const spriteWidth = 120
+    const spriteHeight = 102
+
+    let x = Math.floor(Math.random() * (width - spriteWidth));
+    if (x < leeway) x += leeway;
+    if ((width - x) <= leeway) x -= leeway
+
+    let y = Math.floor(Math.random() * (height - spriteHeight));
+    if (y < leeway) y += leeway;
+    if ((height - y) <= leeway) y -= leeway
+
     const authenticatedContext = useAuthenticatedContext();
-    let x = Math.floor(Math.random() * 500);
-    let y = Math.floor(Math.random() * 300);
 
     const [score, setScore] = React.useState(0);
     const myOnClick = () => {
@@ -17,7 +41,7 @@ export function TaskBoard() {
 
 
     return (
-        <div className="taskboard__container nes-container h-100">
+        <div className="taskboard__container nes-container" ref={ref}>
             <Task x={x} y={y} clickEventHandler={myOnClick} />
         </div>
     );
